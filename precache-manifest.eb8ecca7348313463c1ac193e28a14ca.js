12f65d0a8f5ce3ff63bb8e60dd5780e182ad452e
codepoin <codepoin.id@gmail.com>
2019-05-22 01:55:30 +0700

    Updates

---
+++ precache-manifest.eb8ecca7348313463c1ac193e28a14ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737b5c998566fdc48ae952791da62f20",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "140c3f49433ff29725ad",
+    "url": "/myportfolio/static/css/2.2a88c607.chunk.css"
+  },
+  {
+    "revision": "371853179e6f3dace579",
+    "url": "/myportfolio/static/css/main.ca6043fe.chunk.css"
+  },
+  {
+    "revision": "140c3f49433ff29725ad",
+    "url": "/myportfolio/static/js/2.b00df40b.chunk.js"
+  },
+  {
+    "revision": "371853179e6f3dace579",
+    "url": "/myportfolio/static/js/main.fb533a15.chunk.js"
+  },
+  {
+    "revision": "314136164c1c0c643422",
+    "url": "/myportfolio/static/js/runtime~main.69c47bdd.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/myportfolio/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "a655ba64f13df138ceb90767d6f75d88",
+    "url": "/myportfolio/static/media/rocket.a655ba64.png"
+  }
+]);
